0f5bea39897fd9097f9a3615fd41fdf3fb9caefd
Jasper Hugunin <jasperh@cs.washington.edu>
2018-12-15 19:23:44 -0800

    Complete definition of Char_as_UOT

--- NFA.v
+++ NFA.v
@@ -5,18 +5,28 @@ From Coq Require Import FSets FMaps FMapAVL OrderedTypeEx.
 
 Definition state := Z.
 
-(* Some proofs left as axioms; fill in later *)
 Module Char_as_UOT <: UsualOrderedType.
 Definition t := ascii.
 Definition eq := @eq t.
-Axiom lt : t -> t -> Prop.
+Definition lt (x y : t) := (N_of_ascii x < N_of_ascii y)%N.
 Definition eq_refl := @eq_refl t.
-Definition eq_sym := @eq_sym t.
-Definition eq_trans := @eq_trans t.
-Axiom lt_trans : forall x y z : t, lt x y -> lt y z -> lt x z.
-Axiom lt_not_eq : forall x y : t, lt x y -> ~ eq x y.
-Axiom compare : forall x y : t, Compare lt eq x y.
-Axiom eq_dec : forall x y : t, { eq x y } + { ~ eq x y }.
+Definition eq_sym : forall {x y : t}, eq x y -> eq y x := @eq_sym t.
+Definition eq_trans : forall {x y z : t}, eq x y -> eq y z -> eq x z :=
+  @eq_trans t.
+Definition lt_trans (x y z : t) : lt x y -> lt y z -> lt x z :=
+  N.lt_trans _ _ _.
+Definition lt_not_eq (x y : t) (p : lt x y) (e : eq x y) : False :=
+  N_as_OT.lt_not_eq _ _ p (f_equal N_of_ascii e).
+Definition compare (x y : t) : Compare lt eq x y :=
+  match N_as_OT.compare (N_of_ascii x) (N_of_ascii y) with
+  | LT p => @LT t lt eq x y p
+  | EQ p => @EQ t lt eq x y (eq_trans (eq_trans
+    (eq_sym (ascii_N_embedding x))
+    (f_equal ascii_of_N p))
+    (ascii_N_embedding y))
+  | GT p => @GT t lt eq x y p
+  end.
+Definition eq_dec : forall x y : t, { eq x y } + { ~ eq x y } := ascii_dec.
 End Char_as_UOT.
 
 Module StateSet := FSetAVL.Make(Z_as_OT).
