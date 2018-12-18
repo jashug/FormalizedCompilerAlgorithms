a0a3786597b89ab4ecb2be7773a5ccd0083b85ea
Jasper Hugunin <jasperh@cs.washington.edu>
2018-12-17 19:26:42 -0800

    Cleanup mem= to In

--- GlushkovCorrect.v
+++ GlushkovCorrect.v
@@ -63,8 +63,6 @@ Definition NFA_path_accept (n : NFA) (s : string) : Prop :=
   StateSet.In last n.(finals) /\
   NFA_path n.(next) s first last.
 
-Axiom Admit : forall {T}, T.
-
 Fixpoint fold_left_inc {A B} {P : A -> Prop}
   (f : A -> B -> A) (l : list B) (i : A)
   (IH : forall a b, P a -> P (f a b)) (IH2 : P i)
@@ -73,7 +71,6 @@ Fixpoint fold_left_inc {A B} {P : A -> Prop}
      | nil => IH2
      | cons b l' => fold_left_inc f l' (f i b) IH (IH i b IH2)
      end.
-
 Fixpoint fold_left_inc_in {A B} {P : A -> Prop} {e : B -> B -> Prop}
   (f : A -> B -> A) (l : list B) (i : A)
   (IH : forall a b, P a -> P (f a b)) (b : B)
@@ -84,14 +81,14 @@ Fixpoint fold_left_inc_in {A B} {P : A -> Prop} {e : B -> B -> Prop}
      | InA_cons_tl y H' => fold_left_inc_in f _ _ IH b IH2 H'
      end.
 
-Definition string_app_eq_emp_split (s1 s2 : string)
+(* Definition string_app_eq_emp_split (s1 s2 : string)
   : (s1 ++ s2)%string = EmptyString -> s1 = EmptyString /\ s2 = EmptyString
   := match s1 with
      | EmptyString => fun H => conj eq_refl H
      | String _ _ => fun (H : String _ _ = EmptyString) => ltac:(congruence)
-     end.
+     end. *)
 
-Fixpoint match_emp_l r i s (m : regex_match r s)
+(* Fixpoint match_emp_l r i s (m : regex_match r s)
   : s = EmptyString -> l (annotate_helper i r).(fst) = true
   := match m in regex_match r s
      return s = EmptyString -> l (annotate_helper i r).(fst) = true
@@ -107,42 +104,21 @@ Fixpoint match_emp_l r i s (m : regex_match r s)
        andb_true_intro (conj
          (match_emp_l e i s1 mes1 H1)
          (match_emp_l f _ s2 mfs2 H2))
-     end.
+     end. *)
 
 Definition finals_d r first : StateSet.t :=
   if l r then StateSet.add first (positions (d r))
   else positions (d r).
-Definition finals_ad r i first : StateSet.t :=
-  finals_d (annotate_helper i r).(fst) first.
 
-Axiom state_to_letter_In : forall ls s,
-  StateSet.mem s (positions ls) = true ->
-  exists cs, LetterSet.S.mem (cs , s) ls = true.
-Definition split_finals_ad : forall r i first last next s,
-    regex_match r s -> NFA_path next s first last ->
-    StateSet.mem last (finals_ad r i first) = true ->
-    (exists c, LetterSet.S.mem (c , last)
-                 (d (annotate_helper i r).(fst)) = true) \/
-    (l (annotate_helper i r).(fst) = true /\ first = last) :=
-  fun r i first last next s =>
-    match s with
-    | EmptyString => fun memp pemp _ => or_intror (conj
-      (match_emp_l r i EmptyString memp eq_refl)
-      pemp)
-    | String c s => fun _ _ last_final =>
-      let H1
-       : StateSet.mem last (positions (d (annotate_helper i r).(fst))) = true
-       := Admit in
-      or_introl (state_to_letter_In (d _) last H1)
-    end.
 Definition if_l_first_final r i first (ar := (annotate_helper i r).(fst))
-  : l ar = true -> StateSet.mem first (finals_ad r i first) = true
+  : l ar = true ->
+    StateSet.In first (finals_d (annotate_helper i r).(fst) first)
   := fun H => match eq_sym H in _ = b
-     return StateSet.mem first (if b then _ else _) = true
-     with eq_refl => StateSet.mem_1 (StateSet.add_1 _ eq_refl) end.
+     return StateSet.In first (if b then _ else _)
+     with eq_refl => StateSet.add_1 _ eq_refl end.
 Definition d_in_finals r i first (ar := (annotate_helper i r).(fst))
   : forall lt, LetterSet.S.In lt (d ar) ->
-    StateSet.In lt.(snd) (finals_ad r i first)
+    StateSet.In lt.(snd) (finals_d (annotate_helper i r).(fst) first)
   := fun lt H =>
      let H' : StateSet.In lt.(snd) (positions (d ar)) :=
        match eq_sym (LetterSet.S.fold_1 (d ar) _ _) in _ = ss
@@ -156,6 +132,8 @@ Definition d_in_finals r i first (ar := (annotate_helper i r).(fst))
      return StateSet.In lt.(snd) (if b then _ else _)
      with true => StateSet.add_2 _ H' | false => H' end.
 
+Axiom Admit : forall {T}, T.
+
 Axiom LetterSet_prod_inc : forall lt1 lt2 ls1 ls2,
   LetterSet.S.In lt1 ls1 -> LetterSet.S.In lt2 ls2 ->
   Letter2Set.S.In (lt1 , lt2) (ls1 <*> ls2).
@@ -163,9 +141,9 @@ Axiom LetterSet_prod_inc : forall lt1 lt2 ls1 ls2,
 Definition regex_match_to_path_result r i s next first : Prop :=
   let ar := (annotate_helper i r).(fst) in
   (l ar = true /\ EmptyString = s) \/
-  exists lt : Letter.t,
-  LetterSet.S.mem lt (d ar) = true /\
-  NFA_path next s first lt.(snd).
+  (exists lt : Letter.t,
+   LetterSet.S.In lt (d ar) /\
+   NFA_path next s first lt.(snd)).
 
 Definition regex_match_to_path_result_weaken r1 r2 i1 i2 s next first
   : let ar1 := (annotate_helper i1 r1).(fst) in
@@ -177,9 +155,7 @@ Definition regex_match_to_path_result_weaken r1 r2 i1 i2 s next first
   := fun H1 H2 B => match B with
      | or_introl (conj r1_emp s_emp) => or_introl (conj (H1 r1_emp) s_emp)
      | or_intror (ex_intro _ lt (conj lt_d path)) => or_intror
-       (ex_intro _ lt (conj
-        (LetterSet.S.mem_1 (H2 lt (LetterSet.S.mem_2 lt_d)))
-        path))
+       (ex_intro _ lt (conj (H2 lt lt_d) path))
      end.
 
 Axiom string_app_emp : forall s : string, s = (s ++ "")%string.
@@ -194,7 +170,7 @@ Definition regex_match_to_path_result_compose :
    LetterSet.S.In lt (d ar3)) ->
   (forall lt, LetterSet.S.In lt (d ar2) -> LetterSet.S.In lt (d ar3)) ->
   (l ar1 = true -> regex_match_to_path_result r2 i2 s2 next first) ->
-  (forall lt, LetterSet.S.mem lt (d ar1) = true ->
+  (forall lt, LetterSet.S.In lt (d ar1) ->
    regex_match_to_path_result r2 i2 s2 next lt.(snd)) ->
   regex_match_to_path_result r1 i1 s1 next first ->
   regex_match_to_path_result r3 i3 (s1 ++ s2) next first :=
@@ -212,17 +188,17 @@ Definition regex_match_to_path_result_compose :
       match string_app_emp s1 in _ = s1'
       return regex_match_to_path_result r3 i3 s1' next first
       with eq_refl => or_intror (ex_intro _ lt (conj
-        (LetterSet.S.mem_1 (H2 lt r2_emp (LetterSet.S.mem_2 lt_d)))
+        (H2 lt r2_emp lt_d)
         path1)) : regex_match_to_path_result r3 i3 s1 next first
       end end
     | or_intror (ex_intro _ lt2 (conj lt2_d path2)) =>
       or_intror (ex_intro _ lt2 (conj
-        (LetterSet.S.mem_1 (H3 lt2 (LetterSet.S.mem_2 lt2_d)))
+        (H3 lt2 lt2_d)
         (NFA_path_compose _ _ _ _ _ _ path1 path2)))
     end
   end.
 
-(* forward direction take 2 *)
+(* forward direction *)
 Fixpoint regex_match_to_path (r : regex CharSet.t) (i : Z) (s : string)
     (next : state -> CharMap.t StateSet.t) (first : state)
     (m : regex_match r s)
@@ -234,15 +210,7 @@ Fixpoint regex_match_to_path (r : regex CharSet.t) (i : Z) (s : string)
      forall c, CharSet.In c lt2.(fst) ->
      StateSet.In lt2.(snd) (find_default (next lt1.(snd)) c)) ->
     regex_match_to_path_result r i s next first :=
-  match m in regex_match r s
-  return
-    let ar := (annotate_helper i r).(fst) in
-    (forall lt, LetterSet.S.In lt (p ar) -> forall c, CharSet.In c lt.(fst) ->
-     StateSet.In lt.(snd) (find_default (next first) c)) ->
-    (forall lt1 lt2, Letter2Set.S.In (lt1 , lt2) (f_ ar) ->
-     forall c, CharSet.In c lt2.(fst) ->
-     StateSet.In lt2.(snd) (find_default (next lt1.(snd)) c)) ->
-    regex_match_to_path_result r i s next first
+  match m
   with
   | mEps => fun H1 H2 => or_introl (conj eq_refl eq_refl)
   | mChar cs c c_in_cs => fun H1 H2 =>
@@ -250,9 +218,7 @@ Fixpoint regex_match_to_path (r : regex CharSet.t) (i : Z) (s : string)
      := LetterSet.S.singleton_2 eq_refl in
     let A2 : StateSet.In i (find_default (next first) c)
      := H1 (cs , i) A1 c c_in_cs in
-    or_intror (ex_intro _ (cs , i) (conj
-      (LetterSet.S.mem_1 A1)
-      (ex_intro _ i (conj A2 eq_refl))))
+    or_intror (ex_intro _ (cs , i) (conj A1 (ex_intro _ i (conj A2 eq_refl))))
   | mAltl e f s mes => fun H1 H2 =>
     let Be : regex_match_to_path_result e i s next first
      := regex_match_to_path e i s next first mes
@@ -294,7 +260,7 @@ Fixpoint regex_match_to_path (r : regex CharSet.t) (i : Z) (s : string)
       (fun elast elast_d =>
         regex_match_to_path f _ s2 next elast.(snd) mfs2
             (fun lt lt_in_pf => H2 elast lt (Letter2Set.S.union_3 _
-             (LetterSet_prod_inc _ _ _ _ (LetterSet.S.mem_2 elast_d) lt_in_pf)))
+             (LetterSet_prod_inc _ _ _ _ elast_d lt_in_pf)))
             (fun lt1 lt2 A => H2 lt1 lt2
              (Letter2Set.S.union_2 _ (Letter2Set.S.union_3 _ A))))
       Be
@@ -312,14 +278,14 @@ Fixpoint regex_match_to_path (r : regex CharSet.t) (i : Z) (s : string)
       (fun elast elast_d =>
         regex_match_to_path (Star e) _ s2 next elast.(snd) mses2
           (fun lt lt_in_pf => H2 elast lt (Letter2Set.S.union_3 _
-           (LetterSet_prod_inc _ _ _ _ (LetterSet.S.mem_2 elast_d) lt_in_pf)))
+           (LetterSet_prod_inc _ _ _ _ elast_d lt_in_pf)))
           H2)
       Be
   end.
 
 Definition regex_match_to_path' r s (m : regex_match r s)
   : exists last : state,
-    StateSet.mem last (finals_d (annotate r) 0) = true /\
+    StateSet.In last (finals_d (annotate r) 0) /\
     NFA_path (compile r).(next) s 0 last
   := let B :=
        regex_match_to_path r 1 s (compile r).(next) 0 m
@@ -330,16 +296,14 @@ Definition regex_match_to_path' r s (m : regex_match r s)
          ex_intro _ 0 (conj (if_l_first_final r 1 0 r_emp) eq_refl)
        end
      | or_intror (ex_intro _ lt (conj lt_d path)) =>
-       ex_intro _ lt.(snd) (conj
-         (StateSet.mem_1 (d_in_finals r 1 0 lt (LetterSet.S.mem_2 lt_d)))
-         path)
+       ex_intro _ lt.(snd) (conj (d_in_finals r 1 0 lt lt_d) path)
      end.
 
 (* This is the hard one *)
 Definition regex_match_path_from_zero_iff
   : forall r s, regex_match r s <->
     exists last : state,
-    StateSet.mem last (finals_d (annotate r) 0) = true /\
+    StateSet.In last (finals_d (annotate r) 0) /\
     NFA_path (compile r).(next) s 0 last :=
   fun r s => conj
     (regex_match_to_path' r s)
@@ -349,7 +313,7 @@ Definition regex_match_path_from_zero_iff
 Axiom compiled_NFA_path_zero_iff
   : forall r s,
     (exists last : state,
-     StateSet.mem last (compile r).(finals) = true /\
+     StateSet.In last (compile r).(finals) /\
      NFA_path (compile r).(next) s 0 last)
     <->
     NFA_path_accept (compile r) s.
